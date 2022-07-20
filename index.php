dfe7afe83cf518bc819d5d0222a2915f7e7c8b08
KokuJp <109644461+KokuJp@users.noreply.github.com>
2022-07-20 14:15:23 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>KOKU</title>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="./css/reset.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'>
+
+
+</head>
+ 
+<body>
+    <body>
+    <header>
+    <div class="wrapper">
+        <h1>ようこそ！クオック<span class="color">.</span></h1>
+        <nav>
+            <ul>
+                <li><a href="#">グエン　ヴァン　クオック　学籍番号：2202505</a></li>
+                
+            </ul>
+            
+        </nav>
+    </div>
+</header>
+    
+    
+    <div id="features">
+    <div class="wrapper">
+        <ul>
+           
+
+
+
+
+            <div class="clear"></div>
+        </ul>
+    </div>
+</div>
+    
+    <div id="primary-content">
+    <div class="wrapper">
+        <article>
+            
+        </article>
+    </div>
+</div>
+
+
+    <div id="secondary-content">
+    <div class="wrapper">
+        <article style="background-image: url('images/article-image-1.jpg');">
+            <div class="overlay">
+                <h4>アカウント登録</h4>
+                <p><small>アカウント作成しよう</small></p>
+                <a href="user_register.php" class="more-link">開く</a>
+            </div>
+
+
+        </article>
+
+        <article style="background-image: url('images/article-image-2.jpg');">
+            <div class="overlay">
+                <h4>ログイン</h4>
+                <p><small>登録されたアカウントログイン専用ページ</small></p>
+                <a href="login.php" class="more-link">開く</a>
+            </div>
+        </article><div class="clear"></div>
+
+      <!--   <article style="background-image: url('images/article-image-2.jpg');">
+            <div class="overlay">
+                <h4>出勤と退勤</h4>
+                <p><small>出勤と退勤機能</small></p>
+                <a href="login.php" class="more-link">開く</a>
+            </div>
+        </article><div class="clear"></div> -->
+
+    </div>
+</div>
+    
+    <div id="cta">
+    <div class="wrapper">
+        
+    </div>
+</div>
+    
+
+    <footer>
+    <div class="wrapper">
+        <div id="footer-info">
+            <p>グエン　ヴァン　クオック　の　学籍番号：2202505</p>
+            <p><a href="#">利用規約</a> I <a href="#">プライバシー</a></p>
+        </div>
+        <div id="footer-links">
+            <ul>
+                <li><h5>九州情報大学について</h5></li>
+                <li><a href="#">教員情報公開</a></li>
+                <li><a href="#">Pc-clinic</a></li>
+                <li><a href="#">情報処理室</a></li>
+                <li><a href="#">高校関係者の皆さんへ</a></li>
+            </ul>
+            <ul>
+                <li><h5>情報ネットワーク学科</h5></li>
+                <li><a href="#">受験生の方</a></li>
+                <li><a href="#">在学生の方</a></li>
+                <li><a href="#">卒業生の方</a></li>
+                <li><a href="#">企業の方</a></li>
+            </ul>
+            <ul>
+                <li><h5>データサインコース</h5></li>
+                <li><a href="#">リンク集</a></li>
+                <li><a href="#">サイトマップ</a></li>
+                <li><a href="#">お問い合わせ</a></li>
+                <li><a href="#">教職員</a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+    </div>
+</footer>
+    
+</body>
+</body>
+</html>
+
+
